38cf3ac15d069f80e46a98412704974c6b7f4700
Ilya Molostov <molostov@adn.agency>
2021-11-19 11:55:38 +0700

    simplify and fix errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,122 @@
 const gulp = require('gulp');
-const config = require('./config/gulp.config');
+const scaleImages = require('gulp-scale-images');
+const readMetadata = require('gulp-scale-images/read-metadata');
+const flatMap = require('flat-map').default;
+const del = require('del');
+const glob = require('glob');
 
-function register(options) {
-    return (tasks) => {
-        Object.keys(tasks).forEach((task) => {
-            const tackOptions = {
-                ...options,
-                taskName: task,
+const IMG_GLOB = '**/*.{jpeg,jpg,png,webp}';
+const SRC = 'images';
+const DEST = 'public';
+const IMG_CALLBACK = 'callback';
+const IMG_PROMISE = 'promise';
+
+gulp.task('build:image-callback', () => {
+    const createImages = (file, cb) => {
+        readMetadata(file, (err, meta) => {
+            if (err) {
+                return cb(err);
+            }
+
+            const formats = [meta.format, 'webp'];
+            const sizes = [200, 300, 400];
+            const files = [];
+            const commonScale = {
+                maxWidth: Math.trunc(meta.width * 0.5),
+                withoutEnlargement: false,
+                fit: 'cover',
+                rotate: true,
+                metadata: false,
             };
-            gulp.task(task, require(`./tasks/${tasks[task]}`)(tackOptions));
+            formats.forEach((format) => {
+                sizes.forEach((width) => {
+                    const img = file.clone();
+                    img.scale = {
+                        ...commonScale,
+                        format,
+                        maxWidth: width,
+                    };
+                    img.dirname = `${img.dirname}/${width}`;
+                    files.push(img);
+                });
+            });
+
+            cb(null, files);
         });
     };
-}
 
-register(config)({
-    'clean': 'clean',
-    'build:images': 'build-images',
+    return gulp
+        .src(`${SRC}/${IMG_GLOB}`)
+        .pipe(flatMap(createImages))
+        .pipe(scaleImages())
+        .pipe(gulp.dest(`${DEST}/${IMG_CALLBACK}`));
 });
 
-gulp.task('default', gulp.series('build:images'));
+gulp.task('build:image-promise', () => {
+    const createImages = (file, cb) => {
+        cb(
+            null,
+            new Promise((resolve, reject) => {
+                readMetadata(file, (err, meta) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    const formats = [meta.format, 'webp'];
+                    const sizes = [200, 300, 400];
+                    const files = [];
+                    const commonScale = {
+                        maxWidth: Math.trunc(meta.width * 0.5),
+                        withoutEnlargement: false,
+                        fit: 'cover',
+                        rotate: true,
+                        metadata: false,
+                    };
+                    formats.forEach((format) => {
+                        sizes.forEach((width) => {
+                            const img = file.clone();
+                            img.scale = {
+                                ...commonScale,
+                                format,
+                                maxWidth: width,
+                            };
+                            img.dirname = `${img.dirname}/${width}`;
+                            files.push(img);
+                        });
+                    });
+
+                    resolve(files);
+                });
+            })
+        );
+    };
+
+    return gulp
+        .src(`${SRC}/${IMG_GLOB}`)
+        .pipe(flatMap(createImages))
+        .pipe(scaleImages())
+        .pipe(gulp.dest(`${DEST}/${IMG_PROMISE}`));
+});
+
+gulp.task('clean', () => {
+    return del(DEST);
+});
+
+gulp.task('count', (cb) => {
+    console.log(
+        `Task "build:image-callback" return ${
+            glob.sync(`${DEST}/${IMG_CALLBACK}/${IMG_GLOB}`).length
+        } files.`
+    );
+    console.log(
+        `Task "build:image-promise" return ${
+            glob.sync(`${DEST}/${IMG_PROMISE}/${IMG_GLOB}`).length
+        } files.`
+    );
+    cb();
+});
+
+gulp.task(
+    'default',
+    gulp.series('clean', 'build:image-callback', 'build:image-promise', 'count')
+);
--- tasks/clean.js
+++
@@ -1,9 +0,0 @@
-const del = require('del');
-
-module.exports = (config) => () => {
-    const forDelete = [config.paths.dist];
-    if (config.isDevelopment) {
-        forDelete.push(config.output.ssr);
-    }
-    return del(forDelete);
-};
